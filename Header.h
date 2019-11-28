ea8747afb52853b1e0f95d6b790acf550e64c84f
Lisenok666 <57603720+Lisenok666@users.noreply.github.com>
2019-11-28 19:20:53 +0300

    Update Header.h

--- Header.h
+++ Header.h
@@ -1,31 +1,45 @@
 /*
-	This file sl_list.h
+	This file header.h
 	single linked list header
 	Authora Daniil leskevich and  Oledg Nicolaev
 	Created by NAMELESS TEAM 
 */
-//Matbe some names and function would be changing in future
 #pragma once
-
-typedef struct slist_entry* pslist_entry;
+#include"sl_list_library.c"
+/*typedef struct slist_entry* pslist_entry;
 typedef struct slist* pslist;
 
-struct slist_entry
+
+typedef struct slist_entry
 {
-	pslist_entry next;
-	int value;
+    pslist_entry next;
+    int value;
 } list;
+
 
 struct slist
 {
-	int list_size; // sugar
-	pslist_entry head;
-};
-
+    int list_size; // sugar
+    pslist_entry head;
+} work_list;
+*/
+/*
+ * Creates an empty list
+ * Params: none
+ * Returns pointer to the list
+ */
 pslist slist_new(void);
 
+/*
+ * Deletes list and all the elements
+ * Params: list - pointer to the list
+ * Returns none
+ */
 void slist_delete(pslist list);
 
+/*
+ * TODO
+ */
 int slist_insert(pslist list, int value);
 
 //int slist_remove(pslist list, int value);
