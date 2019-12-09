37c4c708da807f76d6925fa28a3c57c3bab581aa
Lisenok666 <57603720+Lisenok666@users.noreply.github.com>
2019-12-09 23:01:34 +0300

    add header
    I can't add   like a project(((

---
+++ Header_tree.h
@@ -0,0 +1,19 @@
+#pragma once
+struct search_tree
+{
+    int key;
+    struct search_tree* left, * right;
+};
+
+
+struct search_tree* new_tree(int item)
+{}
+
+struct search_tree* dfs(struct search_tree* tree, int key){}
+
+struct search_tree* wfs(struct search_tree* tree, int key) {}
+
+void delete_pare(struct search_tree* dfs(struct search_tree* tree, int key) {}
+
+struct search_tree* insert(struct search_tree* tree, int key){}
+
