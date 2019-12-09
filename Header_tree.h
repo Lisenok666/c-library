e8e5d5f9e9e4e19719352f7c33102f5749d634bd
Lisenok666 <57603720+Lisenok666@users.noreply.github.com>
2019-12-10 01:31:35 +0300

    fix pronlem with header_tree

--- Header_tree.h
+++ Header_tree.h
@@ -1,19 +1,24 @@
+/*
+	This file Header_tree.h
+	tree header
+	Authora Daniil leskevich and  Oledg Nicolaev
+	Created by NAMELESS TEAM 
+*/
 #pragma once
 struct search_tree
 {
     int key;
-    struct search_tree* left, * right;
+    struct search_tree* left;
+    struct search_tree* right;
 };
 
 
-struct search_tree* new_tree(int item)
-{}
+struct search_tree* new_tree(int item);
 
-struct search_tree* dfs(struct search_tree* tree, int key){}
+void dfs(struct search_tree* tree);
 
-struct search_tree* wfs(struct search_tree* tree, int key) {}
+//struct search_tree* wfs(struct search_tree* tree, int key) ;
 
-void delete_pare(struct search_tree* dfs(struct search_tree* tree, int key) {}
+void delete_pare(struct search_tree* tree, int key);
 
-struct search_tree* insert(struct search_tree* tree, int key){}
-
+struct search_tree* insert(struct search_tree* tree, int key);
